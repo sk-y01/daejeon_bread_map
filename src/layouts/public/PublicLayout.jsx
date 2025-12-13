8240c8a4842b50f5f972dcdad7b860c0b3d2e011
ParkSuRim <hollolo123@gmail.com>
2025-12-13 13:00:04 +0900

    chore : logo image

--- src/layouts/public/PublicLayout.jsx
+++ src/layouts/public/PublicLayout.jsx
@@ -1,8 +1,16 @@
-import { Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
 
 const PublicLayout = () => {
   return (
     <>
+      <header>
+        <div className="inner">
+          <h2><img src="/src/assets/images/common/logo.svg" alt="" /></h2>
+          <div className="gnb">
+            <Link to={'/'}>List</Link>
+          </div>
+        </div>
+      </header>
       <main id="main">
         <Outlet />
       </main>
