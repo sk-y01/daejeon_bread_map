bc45ecba9df480d38e3343aeeeb76c6075670ed5
LEEHANEUL <djs000013@naver.com>
2025-12-15 21:04:12 +0900

    feat: 관리자 검색 기능 서버 연동 및 사이드바 UI 개선

--- src/apis/bakeryApi.js
+++ src/apis/bakeryApi.js
@@ -6,77 +6,66 @@
  * URL, 필드명은 백엔드에서 정의한 스펙에 따라 변경될 수 있으므로
  * 실제 API 연결 전 반드시 백엔드에게 확인해야 한다.
  */
-import { api } from "./axiosInstance";
+import { api } from './axiosInstance';
 
 /**
  * ✔ createBakery
- * 
+ *
  * @author        hnlee
  * @method        POST
  * @description   빵집 등록 (multipart/form-data)
  * @param {*}     data  FormData
- * @returns 
  */
 export const createBakery = (data) => {
-  return api.post("/bakeries/add", data, {
+  return api.post('/bakeries/add', data, {
     headers: {
-      "Content-Type": "multipart/form-data",
+      'Content-Type': 'multipart/form-data',
     },
   });
 };
 
 /**
- * 빵집 수정 요청
- *
- * WHY?
- * - PUT인지 PATCH인지는 백엔드 정책을 따라야 한다.
- * - 현재는 임시로 PUT 사용 중.
- *
- * FIXME:
- * - 실제 수정 방식 확인 후 put → patch 변경 가능.
- */
-
-/**
  * ✔ updateBakery
- * 
+ *
  * @author        hnlee
  * @method        PUT
  * @description   빵집 수정 요청 (multipart/form-data)
- * @param {*}     id 
+ * @param {*}     id
  * @param {*}     data  FormData
- * @returns 
  */
 export const updateBakery = (id, data) => {
   return api.put(`/bakeries/update/${id}`, data, {
     headers: {
-      "Content-Type": "multipart/form-data",
+      'Content-Type': 'multipart/form-data',
     },
   });
 };
 
 /**
  * ✔ fetchBakeries
- * 
+ *
  * @author        hnlee
  * @modifier      jjlee
  * @method        GET
- * @description   빵집 목록 조회
- * @param 
- * @returns 
+ * @description   빵집 목록 조회 (검색 / 페이지네이션 대응)
+ *
+ * @param {Object} params
+ * @param {string} params.keyword   검색 키워드
+ * @param {number} params.page      페이지 번호
+ * @param {number} params.limit     페이지당 개수
  */
-export const fetchBakeries = () => {
-  return api.get("/bakeries"); 
+export const fetchBakeries = (params = {}) => {
+  return api.get('/bakeries', { params });
 };
 
 /**
  * ✔ fetchBakeryDetail
- * 
+ *
  * @author        hnlee
  * @modifier      jjlee
  * @method        GET
  * @description   빵집 상세 조회
- * @param {*}     id 
- * @returns 
+ * @param {*}     id
  */
 // TODO : 아직 backend logic 만들지 않음.
 export const fetchBakeryDetail = (id) => {
@@ -85,14 +74,13 @@ export const fetchBakeryDetail = (id) => {
 
 /**
  * ✔ deleteBakery
- * 
+ *
  * @author        hnlee
  * @modifier      jjlee
  * @method        DELETE
  * @description   빵집 삭제 요청 (reason 필수)
- * @param         id
- * @param         reason
- * @returns
+ * @param {*}     id
+ * @param {*}     reason
  */
 export const deleteBakery = (id, reason) => {
   return api.delete(`/bakeries/delete/${id}`, {
--- src/components/admin/Sidebar.jsx
+++ src/components/admin/Sidebar.jsx
@@ -1,6 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from 'react-router-dom';
+import { FaHome, FaStore, FaPlusSquare } from 'react-icons/fa';
 
 const Sidebar = () => {
+  const location = useLocation();
+
   return (
     <aside className="AdminSidebar">
       <h2 className="AdminSidebar__title">관리자 메뉴</h2>
@@ -8,13 +11,44 @@ const Sidebar = () => {
       <nav>
         <ul>
           <li>
-            <Link to="/admin">대시보드</Link>
+            <Link
+              to="/admin"
+              className={`AdminSidebar__item ${
+                location.pathname === '/admin' ? 'is-active' : ''
+              }`}
+            >
+              <FaHome />
+              <span>대시보드</span>
+            </Link>
           </li>
+
           <li>
-            <Link to="/admin/bakery">빵집 리스트 보기</Link>
+            <Link
+              to="/admin/bakery"
+              className={`AdminSidebar__item ${
+                location.pathname.startsWith('/admin/bakery') &&
+                !location.pathname.includes('form')
+                  ? 'is-active'
+                  : ''
+              }`}
+            >
+              <FaStore />
+              <span>빵집 리스트</span>
+            </Link>
           </li>
+
           <li>
-            <Link to="/admin/bakery/form">새 빵집 등록</Link>
+            <Link
+              to="/admin/bakery/form"
+              className={`AdminSidebar__item ${
+                location.pathname.includes('/admin/bakery/form')
+                  ? 'is-active'
+                  : ''
+              }`}
+            >
+              <FaPlusSquare />
+              <span>새 빵집 등록</span>
+            </Link>
           </li>
         </ul>
       </nav>
@@ -23,5 +57,3 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
-
-
