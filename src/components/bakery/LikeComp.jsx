b8fd39209c055ec360e97844e49e0eb0eaf52afd
ParkSuRim <hollolo123@gmail.com>
2026-01-15 23:42:04 +0900

    error : status update error

--- src/components/bakery/LikeComp.jsx
+++ src/components/bakery/LikeComp.jsx
@@ -10,16 +10,32 @@ const LikeComp = () => {
     // 이벤트 버블링 방지(부모 컴포넌트에서 이벤트 발생 방지)
     e.stopPropagation(); 
 
-    setIsLikeActive(active => {
-      const newState = !active;
-      if(newState) {
-        showToast('찜 목록이 추가 되었습니다.', 'success');
-      } else {
-        showToast('찜 목록이 취소 되었습니다.', 'info');
-      }
-      return newState
-    });
+    // Error : LikeComp 렌더링 중 ToastProvider 상태를 업데이트 하고 있음. 
+    // setIsLikeActive의 업데이트 함수 내부에서 showToast를 호출해, 
+    // 렌더링 중 다른 컴포넌트(ToastProvider)의 상태를 변경하려고 함
 
+    // setIsLikeActive(active => {
+    //   const newState = !active;
+    //   if(newState) {
+    //     showToast('찜 목록이 추가 되었습니다.', 'success');
+    //   } else {
+    //     showToast('찜 목록이 취소 되었습니다.', 'info');
+    //   }
+    //   return newState
+    // });
+
+    // 현재 상태를 기반으로 새 상태 계산
+    const newState = !isLikeActive;
+
+    // 상태 업데이트만 수행행
+    setIsLikeActive(newState);
+
+    // 상태 업데이트 후 토스트 메시지 표시(별도로 사이드 이펙트 실행)
+    if(newState) {
+      showToast('찜 목록이 추가 되었습니다.', 'success');
+    } else {
+      showToast('찜 목록이 취소 되었습니다.', 'info');
+    }
   }
 
   return (
