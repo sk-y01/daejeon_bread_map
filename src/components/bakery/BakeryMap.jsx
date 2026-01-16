ed69aeb3fd71ccf76e40e793a62a0d77989459d2
LEEJEONGJAE <st20916@naver.com>
2026-01-16 22:51:38 +0900

    chore : kakao api test

--- src/components/bakery/BakeryMap.jsx
+++ src/components/bakery/BakeryMap.jsx
@@ -214,6 +214,7 @@ const BakeryMap = ({ onBakeryClick }) => {
 
   useEffect(() => {
     if (window.kakao && window.kakao.maps) {
+      console.log('........')
       getLocation()
     } else {
       loadScript()
